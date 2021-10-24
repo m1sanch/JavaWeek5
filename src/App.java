a324e5d83ceaebe7f3289e2a8c8a3608af21c861
Michael <MichaelSanchez1249@gmail.com>
2021-10-24 15:51:56 -0600

    corrections

--- src/App.java
+++ src/App.java
@@ -4,12 +4,14 @@ public class App {
   public static void main(String[] args) {
     
     String instanceTest = "Testing Logger Interface";
-    
+    String instanceTest2 = "Hello World";
     
     //Instance of AsteriskLogger class for log and error method
     AsteriskLogger instanceAsterisk = new AsteriskLogger();
     instanceAsterisk.log(instanceTest);
     instanceAsterisk.error(instanceTest);
+    
+    instanceAsterisk.error(instanceTest2);
     
     //Instance of SpacedLogger class for log and error method
     SpacedLogger instanceSpaced = new SpacedLogger();
